a0fb71192d187cce28c0e54b40fb5981f73f6c9c
deep2555 <deepanshuprashar87@gmail.com>
2025-11-08 16:15:17 +0100

    ok

--- JavaVersionPractice/src/com/deepanshu/interface_practice/MainOperation.java
+++ JavaVersionPractice/src/com/deepanshu/interface_practice/MainOperation.java
@@ -1,17 +1,33 @@
 package com.deepanshu.interface_practice;
 
+import java.util.Scanner;
+
 public class MainOperation {
 
 	public static void main(String[] args) {
-		
-		TransportationCompany transportationCompany = new TransportationCompany();
-		transportationCompany.addVehicle(new Car("1234", "Maruti", 1999));
-		transportationCompany.addVehicle(new Truck("4567", "Ashok Leland", 1998));
-		
-		
-		transportationCompany.startVehicle("1234");
-		transportationCompany.honkVehicle("4567");
 
+//		TransportationCompany transportationCompany = new TransportationCompany();
+//		transportationCompany.addVehicle(new Car("1234", "Maruti", 1999));
+//		transportationCompany.addVehicle(new Truck("4567", "Ashok Leland", 1998));
+//		
+//		
+//		transportationCompany.startVehicle("1234");
+//		transportationCompany.honkVehicle("4567");
+
+		Scanner in = new Scanner(System.in);
+
+		// game loop
+		int mountainH = 0;
+		while (true) {
+			for (int i = 0; i < 8; i++) {
+				mountainH = in.nextInt(); // represents the height of one mountain.
+			}
+
+			// Write an action using System.out.println()
+			// To debug: System.err.println("Debug messages...");
+
+			System.out.println(mountainH); // The index of the mountain to fire on.
+		}
 	}
 
 }
