db60e612db93ec6963d8f6a398054325816e3aaa
deep2555 <deepanshuprashar87@gmail.com>
2025-11-08 23:11:44 +0100

    new update on dialy task

--- JavaVersionPractice/src/com/deepanshu/core/javaPractice/DailyClassTask.java
+++ JavaVersionPractice/src/com/deepanshu/core/javaPractice/DailyClassTask.java
@@ -3,23 +3,105 @@ package com.deepanshu.core.javaPractice;
 public class DailyClassTask {
 
 	public static void main(String[] args) {
-		Book designOfBook = new Book("Design", "Author", 1);
-		Book myBook = new Book(2);
+//		Book designOfBook = new Book("Design", "Author", 1);
+//		Book myBook = new Book(2);
+//
+//		System.out.println("total number of book created: " + Book.getTotalBooks());
+//		designOfBook.borrowBook();
+//		myBook.borrowBook();
+//
+//		designOfBook.borrowBook();
+//		designOfBook.returnBook();
+//		designOfBook.returnBook();
 		
-		System.out.println("total number of book created: "+ Book.getTotalBooks());
-		designOfBook.borrowBook();
-		myBook.borrowBook();
+		Course design = new Course("design of Machine");
+		design.enrolledStudent("Deepanshu Prashar");
+		design.enrolledStudent("Ahbishek");
+		design.enrolledStudent("Kumar");
+		design.enrolledStudent("Karan");
 		
-		designOfBook.borrowBook();
-		designOfBook.returnBook();
-		designOfBook.returnBook();
+		System.out.println("the number of student enroll:"+ design.enrolledStudent);
+		
+		design.unEnrolledStudent("Kumar");
+		System.out.println("the number of student enroll:"+ design.enrolledStudent);
+		
 	}
-	
-	
-	
+
 }
-/* 1) Design the book class and add the instance variable and static variable 
- * simple book class for learning about the properties and its behavior*/
+/*
+ * 1) Design the course class and add the instance variable and static variable
+ * simple book class for learning about the properties and its behavior
+ */
+
+class Course {
+
+	static int maxCapacity;
+	String courseName;
+	int enrolledStudent;
+	
+	//string array to add each student as per the capacity
+	String[] enrolledStu; 
+
+	// static block
+	static {
+		maxCapacity = 4;
+	}
+
+	// constructor
+	public Course(String courseName) {
+		super();
+		this.courseName = courseName;
+		this.enrolledStudent = 0;
+		this.enrolledStu = new String[maxCapacity];
+	}
+
+	// static method
+	static void setMaxCapacity(int capacity) {
+		maxCapacity = capacity;
+	}
+
+	// init method to enroll the student
+	void enrolledStudent(String studentName) {
+		// adding the student name in string array and count up the enrollment
+		enrolledStu[enrolledStudent] = studentName;
+		enrolledStudent++;
+		System.out.println("student is enrolled in the university");
+	}
+
+	// init method to un-enroll the student
+	void unEnrolledStudent(String studentName) {
+		// here remove the student from the string array
+		// with loop only not using any inbuilt and stream option
+		
+		/*1) first step to count how many times that student come
+		 * 2) make new array and add the unremoved student*/
+		int studentCount = 0;
+		String[] unRemoved = new String[enrolledStu.length-studentCount];
+		
+		for(int i=0; i<enrolledStu.length; i++) {
+			if(enrolledStu[i] == studentName) {	
+				studentCount++;
+			}
+		}
+		int j =0;
+		for(int i =0; i<enrolledStu.length; i++) {
+			if(enrolledStu[i] != studentName) {
+				unRemoved[j] = enrolledStu[i];
+				j++;
+				
+			}
+		
+		}
+		enrolledStudent--;
+		System.out.println("student is unenrolled from the university");
+	}
+
+}
+
+/*
+ * 1) Design the book class and add the instance variable and static variable
+ * simple book class for learning about the properties and its behavior
+ */
 
 class Book {
 
@@ -68,16 +150,17 @@ class Book {
 
 		if (!isBorrowed) {
 			this.isBorrowed = true;
-			System.out.println("Enjoy Book"+ this.bookTitle);
+			System.out.println("Enjoy Book" + this.bookTitle);
 		} else {
 			System.out.println("The book is already Borrowed");
 		}
 	}
-	void returnBook(){
-		if(isBorrowed) {
+
+	void returnBook() {
+		if (isBorrowed) {
 			this.isBorrowed = false;
 			System.out.println("Hope you enjoy my book");
-		}else {
+		} else {
 			System.out.println("this book is still in the library");
 		}
 	}
