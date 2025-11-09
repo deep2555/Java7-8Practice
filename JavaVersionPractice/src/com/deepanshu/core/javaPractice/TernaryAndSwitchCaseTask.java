4a3785e4e8d92f2926d6181a4a165d166cdb9a53
deep2555 <deepanshuprashar87@gmail.com>
2025-11-09 12:32:32 +0100

    added in the file by deepanshu 09-11-25

--- JavaVersionPractice/src/com/deepanshu/core/javaPractice/TernaryAndSwitchCaseTask.java
+++ JavaVersionPractice/src/com/deepanshu/core/javaPractice/TernaryAndSwitchCaseTask.java
@@ -7,21 +7,32 @@ public class TernaryAndSwitchCaseTask {
 
 	public static void main(String[] args) {
 
-		int[] userIP = getUserInput(2);
-		checkMinimum(userIP);
+		// int[] userIP = getUserInput(2);
+		// checkMinimum(userIP);
+
+		int usrIP = getInput();
+		checkOddEven(usrIP);
 	}
 
-	
-	// check the minimum number between two given number by ternary operator
+	// Task 1 ) check the minimum number between two given number by ternary
+	// operator
 	public static void checkMinimum(int[] userIP) {
 		// TODO Auto-generated method stub
 		// simple value added dynamically
 		// using ternary operator
-		int minimumOP= userIP[0] < userIP[1]? userIP[0]: userIP[1];
+		int minimumOP = userIP[0] < userIP[1] ? userIP[0] : userIP[1];
 		System.out.println(minimumOP + ": is the minimum number");
 	}
 
-	// normal method to get input from user
+	// Task 2) program to find if the given number is even or odd using ternary
+	// operator
+	public static void checkOddEven(int number) {
+		String checkODEV = number % 2 == 0 ? "even" : "Odd";
+		System.out.println("the given number is : " + checkODEV);
+
+	}
+
+	// normal method to get input from user using loop
 	public static int[] getUserInput(int user) {
 
 		Scanner sc = new Scanner(System.in);
@@ -31,6 +42,19 @@ public class TernaryAndSwitchCaseTask {
 			System.out.println("enter your number: ");
 			number[i] = sc.nextInt();
 		}
+		sc.close();
+		return number;
+
+	}
+
+	// normal method to get input from user without using loop
+	public static int getInput() {
+
+		Scanner sc = new Scanner(System.in);
+		int number = 0;
+
+		System.out.println("enter your number: ");
+		number = sc.nextInt();
 		sc.close();
 		return number;
 
