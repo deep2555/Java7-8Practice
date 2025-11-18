a5183643b5ff296fefb977fe6c7142545a076488
deep2555 <deepanshuprashar87@gmail.com>
2025-11-18 17:19:33 +0100

    new changes

--- JavaVersionPractice/src/com/deepanshu/core/javaPractice/TestSeries3.java
+++ JavaVersionPractice/src/com/deepanshu/core/javaPractice/TestSeries3.java
@@ -7,23 +7,19 @@ public class TestSeries3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String pass;
-		String presentPass = "deep";
+		final String presentPass = "deep";
 		do {
 			System.out.println("enter the password to check :");
 			pass = sc.next();
-		} while (passwordEncoder(pass, presentPass));
+		} while (!passwordEncoder(pass, presentPass));
+		System.out.println("access grated");
 
 	}
 
 	// password checker until valid password is enter (DO-WHILE)
 	public static boolean passwordEncoder(String password, String presentPass) {
 		// using the do while loop
-
-		System.out.println("inside the do loop ");
-		if (password.equalsIgnoreCase(presentPass)) {
-			return true;
-		}
-		return false;
+		return password.equals(presentPass);
 
 	}
 }
